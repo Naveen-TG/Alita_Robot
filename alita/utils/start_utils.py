3fffd6206d2923e0e483e9e87de8181a5e69f7ea
Muhammedalthaf174 <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-01-23 19:37:39 +0530

    Update start_utils.py

--- alita/utils/start_utils.py
+++ alita/utils/start_utils.py
@@ -74,8 +74,8 @@ async def gen_start_kb(q: Message or CallbackQuery):
             [
                 (f"🌐 {(tlang(q, 'start.language_btn'))}", "chlang"),
                 (
-                    f"🗃️ {(tlang(q, 'start.source_code'))}",
-                    "https://github.com/DivideProjects/Alita_Robot",
+                    f" {(tlang(q, 'start.source_code'))}",
+                    "milnabotchannel",
                     "url",
                 ),
             ],
