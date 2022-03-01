5e00363d898b5ffe7bed3289d81e3f4230d836c1
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-03-01 09:36:36 +0530

    Create tagall.py

---
+++ alita/plugins/tagall.py
@@ -0,0 +1,13 @@
+
+
+__mod_name__ = "Tag all"
+__help__ = """
+──「 Mention all members 」──
+
+Can Be a Mention Bot for your group.
+
+Only admins can tag all.  here is a list of commands
+
+❂ /tagall or @all (reply to message or add another message) To mention all members in your group, without exception.
+❂ /cancel for canceling the mention-all.
+"""
