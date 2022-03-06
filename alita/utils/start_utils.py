b897a3c4c2e1f4e9e1b436eeb53b7132d7766f78
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-03-06 07:43:30 +0530

    Update start_utils.py

--- alita/utils/start_utils.py
+++ alita/utils/start_utils.py
@@ -75,7 +75,7 @@ async def gen_start_kb(q: Message or CallbackQuery):
                 (f"🌐 {(tlang(q, 'start.language_btn'))}", "chlang"),
                 (
                     f"🗃️ {(tlang(q, 'start.source_code'))}",
-                    "https://t.me/milnabotchannel",
+                    "https://t.me/tezzasupportchannel",
                     "url",
                 ),
             ],
