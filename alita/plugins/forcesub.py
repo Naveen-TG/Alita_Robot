1686d109bcf6c94cf9211e53bc350280301aba47
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-03-10 07:59:44 +0530

    Update forcesub.py

--- alita/plugins/forcesub.py
+++ alita/plugins/forcesub.py
@@ -10,9 +10,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from ShuKurenaiXRoBot import DRAGONS as SUDO_USERS
-from ShuKurenaiXRoBot import pbot
-from ShuKurenaiXRoBot.modules.sql import forceSubscribe_sql as sql
+from alita import DRAGONS as SUDO_USERS
+from alita import pbot
+from alita.modules.sql import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
@@ -188,16 +188,6 @@ __help__ = """
 ❂ /fsub - To get the current settings.
 ❂ /fsub disable - To turn of ForceSubscribe..
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
-❂ /fsub clear - To unmute all members who muted by me.
-*Federation*
-Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
-But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
-*No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
-You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
-*Commands:*\n
-Feds are now divided into 3 sections for your ease.
-• `/fedownerhelp`*:* Provides help for fed creation and owner only commands
-• `/fedadminhelp`*:* Provides help for fed administration commands
-• `/feduserhelp`*:* Provides help for commands anyone can use
+❂ /fsub clear - To unmute all members who muted by me. 
 """
-__mod_name__ = "F-Sub/Feds"
+__mod_name__ = "F-Sub"
