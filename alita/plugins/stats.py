ac2bce253bb7e6b10c05b8825a09a7ae7a3cb60d
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-06-13 08:24:02 +0530

    Update stats.py

--- alita/plugins/stats.py
+++ alita/plugins/stats.py
@@ -52,35 +52,15 @@ async def get_stats(_, m: Message):
     warns_db = Warns
     warns_settings_db = WarnSettings
 
-    replymsg = await m.reply_text("<b><i>Fetching Stats...</i></b>", quote=True)
-    rply = (
-        f"<b>Users:</b> <code>{(userdb.count_users())}</code> in <code>{(chatdb.count_chats())}</code> chats\n"
-        f"<b>Anti Channel Pin:</b> <code>{(pinsdb.count_chats('antichannelpin'))}</code> enabled chats\n"
-        f"<b>Clean Linked:</b> <code>{(pinsdb.count_chats('cleanlinked'))}</code> enabled chats\n"
-        f"<b>Filters:</b> <code>{(fldb.count_filters_all())}</code> in <code>{(fldb.count_filters_chats())}</code> chats\n"
-        f"    <b>Aliases:</b> <code>{(fldb.count_filter_aliases())}</code>\n"
-        f"<b>Blacklists:</b> <code>{(bldb.count_blacklists_all())}</code> in <code>{(bldb.count_blackists_chats())}</code> chats\n"
-        f"    <b>Action Specific:</b>\n"
-        f"        <b>None:</b> <code>{(bldb.count_action_bl_all('none'))}</code> chats\n"
-        f"        <b>Kick</b> <code>{(bldb.count_action_bl_all('kick'))}</code> chats\n"
-        f"        <b>Warn:</b> <code>{(bldb.count_action_bl_all('warn'))}</code> chats\n"
-        f"        <b>Ban</b> <code>{(bldb.count_action_bl_all('ban'))}</code> chats\n"
-        f"<b>Rules:</b> Set in <code>{(rulesdb.count_chats_with_rules())}</code> chats\n"
-        f"    <b>Private Rules:</b> <code>{(rulesdb.count_privrules_chats())}</code> chats\n"
-        f"<b>Warns:</b> <code>{(warns_db.count_warns_total())}</code> in <code>{(warns_db.count_all_chats_using_warns())}</code> chats\n"
-        f"    <b>Users Warned:</b> <code>{(warns_db.count_warned_users())}</code> users\n"
-        f"    <b>Action Specific:</b>\n"
-        f"        <b>Kick</b>: <code>{(warns_settings_db.count_action_chats('kick'))}</code>\n"
-        f"        <b>Mute</b>: <code>{(warns_settings_db.count_action_chats('mute'))}</code>\n"
-        f"        <b>Ban</b>: <code>{warns_settings_db.count_action_chats('ban')}</code>\n"
-        f"<b>Notes:</b> <code>{(notesdb.count_all_notes())}</code> in <code>{(notesdb.count_notes_chats())}</code> chats\n"
-        f"    <b>Private Notes:</b> <code>{(notesettings_db.count_chats())}</code> chats\n"
-        f"<b>GBanned Users:</b> <code>{(gbandb.count_gbans())}</code>\n"
-        f"<b>Welcoming Users in:</b> <code>{(grtdb.count_chats('welcome'))}</code> chats"
-        f"<b>Approved People</b>: <code>{(appdb.count_all_approved())}</code> in <code>{(appdb.count_approved_chats())}</code> chats\n"
-        f"<b>Disabling:</b> <code>{(dsbl.count_disabled_all())}</code> items in <code>{(dsbl.count_disabling_chats())}</code> chats.\n"
-        "<b>Action:</b>\n"
-        f"     <b>Del:</b> Applied in <code>{(dsbl.count_action_dis_all('del'))}</code> chats.\n"
-    )
+@sudo_plus
+def stats(update: Update, context: CallbackContext):
+    stats = "<b>╔═━「 Current Tezza Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
+    result += "\n<b>╘═━「 Powered By Tezzasupportgroup 」</b>"
+    update.effective_message.reply_text(
+        result,
+        parse_mode=ParseMode.HTML, 
+        disable_web_page_preview=True
+   )
     await replymsg.edit_text(rply, parse_mode="html")
     return
